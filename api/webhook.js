fd5737a0351ae1fc9249c79c9f9a371f2ec0603b
Saleck_bz <bababz123123@gmail.com>
2026-09-18 23:11:27 +0000

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -419,4 +419,4 @@ export default async function handler(req, res) {
     { inline_keyboard: [[{ text: '📋 نسخ', url: link2 }]] });
 
   res.status(200).send('OK');
-      }
+                              }
